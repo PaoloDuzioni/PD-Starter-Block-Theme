5d17f68995ad953661fb20ddddeedbfdaa6e14a7
Paolo Duzioni <paolo.duzioni@gmail.com>
2024-12-19 14:07:46 +0100

    refactor options for header.twig and footer.twig

--- core/theme_setup.php
+++ core/theme_setup.php
@@ -1,5 +1,4 @@
 <?php
-add_filter('timber/context', 'add_to_context');
 
 /**
  * Global Timber context.
@@ -7,10 +6,15 @@ add_filter('timber/context', 'add_to_context');
  * @param array $context Global context variables.
  *
  * - Menu registration
+ * - ACF Options
  */
+add_filter('timber/context', 'add_to_context');
+
 function add_to_context($context)
 {
     $context['menu'] = Timber::get_menu('main-menu', ['depth' => 2]);
+    
+    $context['options'] = get_fields('option');
     
     return $context;
 }
