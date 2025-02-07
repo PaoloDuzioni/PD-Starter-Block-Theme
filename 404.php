e785908eb82872241e28c6c5beca32d18d0a6c31
Paolo Duzioni <paolo.duzioni@gmail.com>
2025-02-07 12:20:40 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,16 +1,10 @@
 <?php if (!defined('ABSPATH')) exit;
 
-get_header(); ?>
-<!--TODO: add a 404 template-->
-<section id="page-404" class="wrap-internals container text-center">
-    <img class="image" src="<?php echo get_template_directory_uri(); ?>/dist/images/img-404.svg" alt="Page not found">
-    <div class="section-title small bold">
-        <h1>404 - Pagina non trovata</h1>
-    </div>
-    <p><?php _e("Sei sicuro quello che stai cercando si trova su questo sito?", 'pdtheme'); ?></p>
-    <a class="button" href="<?php echo esc_attr(home_url()); ?>">
-        <?php _e("Torna alla home", 'pdtheme'); ?>
-    </a>
-</section>
+$context = Timber::context();
+$context['image'] = get_template_directory_uri() . "/dist/images/img-404.svg";
+$context['title'] = __("404 - Pagina non trovata", 'pdtheme');
+$context['text'] = __("Sei sicuro quello che stai cercando si trova su questo sito?", 'pdtheme');
+$context['button_text'] = __("Torna alla home", 'pdtheme');
+$context['home_url'] =  esc_attr(home_url());
 
-<?php get_footer(); ?>
+Timber::render('404.twig', $context);
