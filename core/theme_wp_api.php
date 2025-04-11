3cf7a609e996494477dee56a15f8187885e97344
Paolo Duzioni <paolo.duzioni@gmail.com>
2025-04-11 17:50:57 +0200

    Update theme_wp_api.php

--- core/theme_wp_api.php
+++ core/theme_wp_api.php
@@ -1,4 +1,6 @@
-<?php if (!defined('ABSPATH')) exit;
+<?php if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * THEME WP API CUSTOM ENDPOINTS
@@ -6,41 +8,40 @@
  * https://developer.wordpress.org/rest-api/extending-the-rest-api/adding-custom-endpoints/
  */
 
-
 /**
  * GET POSTS END POINT
  *
  * API URL: http://yoursite.com/wp-json/pd/v1/posts/
  */
-add_action('rest_api_init', function () {
-    /**
-     * Registering of a new route
-     *
-     * https://developer.wordpress.org/reference/functions/register_rest_route/
-     *
-     * @param1 - namespace
-     * @param2 - route
-     * @param3 - args
-     */
-    register_rest_route('pd/v1', '/posts', array(
-        'methods' => 'GET',
-        'callback' => 'pd_get_posts',
-        'permission_callback' => '__return_true'
-    ));
-});
+//add_action('rest_api_init', function () {
+//    /**
+//     * Registering of a new route
+//     *
+//     * https://developer.wordpress.org/reference/functions/register_rest_route/
+//     *
+//     * @param1 - namespace
+//     * @param2 - route
+//     * @param3 - args
+//     */
+//    register_rest_route('pd/v1', '/posts', array(
+//        'methods' => 'GET',
+//        'callback' => 'pd_get_posts',
+//        'permission_callback' => '__return_true'
+//    ));
+//});
 
 /*** Callback for the new route ***/
-function pd_get_posts($data): array | null
-{
-    //print debug log to php error log
-    error_log(print_r($data, true));
-
-    // Perform your query
-    $posts = get_posts();
-
-    // Return the results
-    if (empty($posts)) {
-        return null;
-    }
-    return $posts;
-}
+//function pd_get_posts($data): array | null
+//{
+//    //print debug log to php error log
+//    error_log(print_r($data, true));
+//
+//    // Perform your query
+//    $posts = get_posts();
+//
+//    // Return the results
+//    if (empty($posts)) {
+//        return null;
+//    }
+//    return $posts;
+//}
